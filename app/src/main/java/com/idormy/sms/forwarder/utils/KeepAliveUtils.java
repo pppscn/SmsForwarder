7bab5e71e88b0edda19de9112f9cf11793287dcd
miaoc <luoyangchong@163.com>
2021-07-16 11:52:50 +0800

    增加电池优化白名单设置和权限

---
+++ app/src/main/java/com/idormy/sms/forwarder/utils/KeepAliveUtils.java
@@ -0,0 +1,38 @@
+package com.idormy.sms.forwarder.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import com.idormy.sms.forwarder.R;
+
+public class KeepAliveUtils {
+
+    public static boolean isIgnoreBatteryOptimization(Activity activity) {
+        PowerManager powerManager = (PowerManager) activity.getSystemService(Context.POWER_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M && powerManager != null) {
+            return powerManager.isIgnoringBatteryOptimizations(activity.getPackageName());
+        } else {
+            return true;
+        }
+    }
+
+    public static void ignoreBatteryOptimization(Activity activity) {
+        if (isIgnoreBatteryOptimization(activity)) {
+            return;
+        }
+        Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+        intent.setData(Uri.parse("package:" + activity.getPackageName()));
+        ResolveInfo resolveInfo = activity.getPackageManager().resolveActivity(intent, 0);
+        if (resolveInfo != null) {
+            activity.startActivity(intent);
+        } else {
+            Toast.makeText(activity, R.string.unsupport, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
