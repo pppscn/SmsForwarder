7246af10b5663033372c049c8df9949b9ea90661
Naccl <i@naccl.top>
2022-03-06 19:02:53 +0800

    优化：抽取电池状态信息工具类

--- app/src/main/java/com/idormy/sms/forwarder/service/BatteryService.java
+++ app/src/main/java/com/idormy/sms/forwarder/service/BatteryService.java
@@ -6,7 +6,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -14,6 +13,7 @@ import com.idormy.sms.forwarder.MyApplication;
 import com.idormy.sms.forwarder.model.vo.SmsHubVo;
 import com.idormy.sms.forwarder.model.vo.SmsVo;
 import com.idormy.sms.forwarder.sender.SendUtil;
+import com.idormy.sms.forwarder.utils.BatteryUtils;
 import com.idormy.sms.forwarder.utils.SettingUtil;
 import com.idormy.sms.forwarder.utils.SmsHubActionHandler;
 
@@ -74,7 +74,7 @@ public class BatteryService extends Service {
             int levelCur = intent.getIntExtra("level", 0);
             int levelPre = SettingUtil.getBatteryLevelCurrent();
             if (levelCur != levelPre) {
-                String msg = batteryReceiver(intent);
+                String msg = BatteryUtils.getBatteryInfo(intent);
                 SettingUtil.setBatteryLevelCurrent(levelCur);
 
                 int levelMin = SettingUtil.getBatteryLevelAlarmMin();
@@ -103,98 +103,14 @@ public class BatteryService extends Service {
             if (SettingUtil.getSwitchEnableBatteryReceiver()) {
                 int oldStatus = SettingUtil.getBatteryStatus();
                 if (status != oldStatus) {
-                    String msg = batteryReceiver(intent);
+                    String msg = BatteryUtils.getBatteryInfo(intent);
                     SettingUtil.setBatteryStatus(status);
-                    msg = "【充电状态】发生变化：" + getStatus(oldStatus) + " → " + getStatus(status) + msg;
+                    msg = "【充电状态】发生变化：" + BatteryUtils.getStatus(oldStatus) + " → " + BatteryUtils.getStatus(status) + msg;
                     sendMessage(context, msg);
                 }
             }
         }
     };
-
-    @SuppressLint("DefaultLocale")
-    private String batteryReceiver(Intent intent) {
-        Log.i(TAG, "BatteryReceiver--------------");
-        String action = intent.getAction();
-        Log.i(TAG, " 0 action:" + action);
-        Log.i(TAG, "ACTION_BATTERY_CHANGED");
-        int status = intent.getIntExtra("status", 0);
-        int health = intent.getIntExtra("health", 0);
-        //boolean present = intent.getBooleanExtra("present", false);
-        int levelCur = intent.getIntExtra("level", 0);
-        int scale = intent.getIntExtra("scale", 0);
-        //int icon_small = intent.getIntExtra("icon-small", 0);
-        int plugged = intent.getIntExtra("plugged", 0);
-        int voltage = intent.getIntExtra("voltage", 0);
-        int temperature = intent.getIntExtra("temperature", 0);
-        //String technology = intent.getStringExtra("technology");
-
-        String msg = "";
-        msg += "\n剩余电量：" + levelCur + "%";
-
-        if (scale > 0) msg += "\n充满电量：" + scale + "%";
-
-        if (voltage > 0) msg += "\n当前电压：" + String.format("%.2f", voltage / 1000F) + "V";
-
-        if (temperature > 0) msg += "\n当前温度：" + String.format("%.2f", temperature / 10F) + "℃";
-
-        msg += "\n电池状态：" + getStatus(status);
-
-        if (health > 0) msg += "\n健康度：" + getHealth(health);
-
-        switch (plugged) {
-            case BatteryManager.BATTERY_PLUGGED_AC:
-                msg += "\n充电器：AC";
-                break;
-            case BatteryManager.BATTERY_PLUGGED_USB:
-                msg += "\n充电器：USB";
-                break;
-            case BatteryManager.BATTERY_PLUGGED_WIRELESS:
-                msg += "\n充电器：无线";
-                break;
-        }
-
-        Log.i(TAG, msg);
-        return msg;
-    }
-
-    //电池状态
-    private String getStatus(int status) {
-        switch (status) {
-            case BatteryManager.BATTERY_STATUS_CHARGING:
-                return "充电中";
-            case BatteryManager.BATTERY_STATUS_DISCHARGING:
-                return "放电中";
-            case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
-                return "未充电";
-            case BatteryManager.BATTERY_STATUS_FULL:
-                return "充满电";
-            case BatteryManager.BATTERY_STATUS_UNKNOWN:
-            default:
-                return "未知";
-        }
-    }
-
-    //健康度
-    private String getHealth(int health) {
-        switch (health) {
-            case 2:
-                return "良好";
-            case 3:
-                return "过热";
-            case 4:
-                return "没电";
-            case 5:
-                return "过电压";
-            case 6:
-                return "未知错误";
-            case 7:
-                return "温度过低";
-            default:
-            case 1:
-                return "未知";
-        }
-    }
 
     //发送信息
     private void sendMessage(Context context, String msg) {
