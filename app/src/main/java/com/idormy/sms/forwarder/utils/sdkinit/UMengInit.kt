a21e604571441dbe400059d872b7dce4a864c4b0
pppscn <35696959@qq.com>
2022-09-21 14:03:39 +0800

    精简：去掉美团多渠道打包 升级：权限请求框架 XXPermissions

--- app/src/main/java/com/idormy/sms/forwarder/utils/sdkinit/UMengInit.kt
+++ app/src/main/java/com/idormy/sms/forwarder/utils/sdkinit/UMengInit.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import android.content.Context
 import com.idormy.sms.forwarder.BuildConfig
 import com.idormy.sms.forwarder.utils.SettingUtils.Companion.isAgreePrivacy
-import com.meituan.android.walle.WalleChannelReader
+//import com.meituan.android.walle.WalleChannelReader
 import com.umeng.analytics.MobclickAgent
 import com.umeng.commonsdk.UMConfigure
 import com.xuexiang.xui.XUI
@@ -79,7 +79,8 @@ class UMengInit private constructor() {
          * @return
          */
         private fun getChannel(context: Context?): String? {
-            return WalleChannelReader.getChannel(context!!, DEFAULT_CHANNEL_ID)
+            //return WalleChannelReader.getChannel(context!!, DEFAULT_CHANNEL_ID)
+            return DEFAULT_CHANNEL_ID
         }
     }
 
