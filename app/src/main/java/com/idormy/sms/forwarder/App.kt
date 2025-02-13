23acf01c61503eee33e2cfde02620997200f665f
pppscn <35696959@qq.com>
2025-02-13 19:18:05 +0800

    新增： `{{PHONE_AREA}}` 标签用于反查`{{FROM}}`对应的归属地 #564

--- app/src/main/java/com/idormy/sms/forwarder/App.kt
+++ app/src/main/java/com/idormy/sms/forwarder/App.kt
@@ -417,6 +417,7 @@ class App : Application(), CactusCallback, Configuration.Provider by Core {
                 getString(R.string.tag_card_slot) to getString(R.string.insert_tag_card_slot),
                 getString(R.string.tag_card_subid) to getString(R.string.insert_tag_card_subid),
                 getString(R.string.tag_contact_name) to getString(R.string.insert_tag_contact_name),
+                getString(R.string.tag_phone_area) to getString(R.string.insert_tag_phone_area),
             )
         )
         CALL_TAG_MAP.clear()
@@ -428,6 +429,7 @@ class App : Application(), CactusCallback, Configuration.Provider by Core {
                 getString(R.string.tag_card_subid) to getString(R.string.insert_tag_card_subid),
                 getString(R.string.tag_call_type) to getString(R.string.insert_tag_call_type),
                 getString(R.string.tag_contact_name) to getString(R.string.insert_tag_contact_name),
+                getString(R.string.tag_phone_area) to getString(R.string.insert_tag_phone_area),
             )
         )
         APP_TAG_MAP.clear()
